f55b27c6ddd1a4b0f1bd5866f14c4a9f128b4820
cocoprog <morales.federico14@gmail.com>
2022-09-15 15:05:09 -0300

    v1

--- src/components/Counters/index.jsx
+++ src/components/Counters/index.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react"
 
 export function Counters () {
+    const stock = 5
     const [count, setCount] = useState(0)
 
-    const decreaseCount = () => {setCount (count => count -1)}
-    const increaseCount = () => {setCount (count => count +1)}
+    // const decreaseCount = () => {setCount (count => count -1)}
+    // const increaseCount = () => {setCount (count => count +1)}
+    const increaseCount = () => count < stock ? setCount (count +1) : alert (`se alcanzo el maximo de stock disponible`)
+    const decreaseCount = () => count > 0 ? setCount (count -1) : alert (`no se pueden introducir valores negativos`)
 
     return (
         <div className="counters-container">
